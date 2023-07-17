82790fe2fc6d13dd74781456f1c19ce045797472
Zach <zachhutz0@gmail.com>
2023-07-16 20:53:24 -0400

    cleaned up router and added register route

--- src/App.js
+++ src/App.js
@@ -1,29 +1,28 @@
-import React, { useState } from 'react';
-import './App.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Login from './components/Login/Login';
-import Dashboard from './components/Dashboard/Dashboard';
+import React, { useState } from "react";
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Login from "./components/Login/Login";
+import Register from "./components/Register/Register";
+import Dashboard from "./components/Dashboard/Dashboard";
 
 function App() {
-  const [token, setToken] = useState();
-  if(!token) {
-    return <Login setToken={setToken} />
-  }
+	// const [token, setToken] = useState();
+	// if (!token) {
+	// 	return <Login setToken={setToken} />;
+	// }
 
-  return (
-    <div className="App"> 
-     <div className="wrapper">
-      <h1>Application</h1>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/dashboard">
-            <Dashboard />
-          </Route>
-        </Routes>
-      </BrowserRouter>
-    </div>
-    </div>
-  );
+	return (
+		<div className="App">
+			<div className="wrapper">
+				<BrowserRouter>
+					<Routes>
+						<Route path="/" element={<Login />} />
+						<Route path="/register" element={<Register />} />
+					</Routes>
+				</BrowserRouter>
+			</div>
+		</div>
+	);
 }
 
 export default App;
