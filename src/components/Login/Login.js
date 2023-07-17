2b7e9c4fe2d590bcf2accead16ba249a21c22c2d
Zach <zachhutz0@gmail.com>
2023-07-16 20:55:05 -0400

    changed to return a proper React fragment, added registration link

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,15 +1,34 @@
-import './Login.css';
+import "./Login.css";
 
-function  Login() {
-  return (
-        <form className="Login-container" action="">
-            <label for="username">Username:</label>
-            <input type="text" id="username" name="username" placeholder='Username' /><br/>
-            <label for="password">Password:</label>
-            <input type="password" id="password" name="password" placeholder='Password' /><br/>
-            <input className="login-button" type="submit" value="Submit" />
-        </form> 
-  );
+function Login() {
+	return (
+		<>
+			<form className="Login-container" action="">
+				<label for="username">Username:</label>
+				<input
+					type="text"
+					id="username"
+					name="username"
+					placeholder="Username"
+				/>
+				<br />
+				<label for="password">Password:</label>
+				<input
+					type="password"
+					id="password"
+					name="password"
+					placeholder="Password"
+				/>
+				<br />
+				<input className="login-button" type="submit" value="Submit" />
+			</form>
+			<p>
+				Don't have an account? Register{" "}
+				<a href="http://localhost:3000/register">here</a>.
+				{/* TODO: replace with more permanent solution */}
+			</p>
+		</>
+	);
 }
 
 export default Login;
