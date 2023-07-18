54df604a6ab42047088b84c920a6d23aa50c71e9
Zach <zachhutz0@gmail.com>
2023-07-18 19:21:04 -0400

    clean up login

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -2,9 +2,6 @@ import "./Login.css";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
-
-
-
 function Login() {
 	const [username, setUsername] = useState("");
 	const [password, setPassword] = useState("");
@@ -26,8 +23,8 @@ function Login() {
 			});
 			const data = await res.json();
 			setMessage(data.message);
-			localStorage.setItem("refresh",data.refresh);
-			localStorage.setItem("token",data.token);
+			localStorage.setItem("refresh", data.refresh);
+			localStorage.setItem("token", data.token);
 			if (res.ok) {
 				setMessage("");
 				navigate("/");
@@ -35,44 +32,43 @@ function Login() {
 		} catch (e) {
 			console.log(e);
 		}
-	}
+	};
 	return (
-		<>	
-			<div className="main-wrapper">
-				<div className="main-container">
-					<div className="main-title">
-						<img src={ require("./logo-black.png") } alt="" />
-						<h1>LOGIN</h1>
-					</div>
+		<>
+			<div className="login-wrapper">
+				<div className="login-container">
+					<img src={require("./logo-black.png")} alt=""></img>
 					<form onSubmit={submitForm}>
-							<input
-								type="text"
-								id="username"
-								name="username"
-								placeholder="Username"
-								onChange={e => setUsername(e.target.value)}
-							/>
-							<br />
-							<input
-								type="password"
-								id="password"
-								name="password"
-								placeholder="Password"
-								onChange={e => setPassword(e.target.value)}
-							/>
-							<div className="main-error">{message && <p>{message}</p>}</div>
-							<input className="main-button" type="submit" value="Login" />
-							<br />
-							<a href="http://localhost:3000/dashboard">Temp button to go to dashboard</a>.
+						<input
+							type="text"
+							id="username"
+							name="username"
+							placeholder="Username"
+							onChange={(e) => setUsername(e.target.value)}
+						/>
+						<br />
+						<input
+							type="password"
+							id="password"
+							name="password"
+							placeholder="Password"
+							onChange={(e) => setPassword(e.target.value)}
+						/>
+						<br />
+						<input className="login-button" type="submit" value="Submit" />
+						<br />
+						<a href="http://localhost:3000/dashboard">
+							Temp button to go to dashboard
+						</a>
+						.
 					</form>
 				</div>
 				<div className="register">
-					<p>
-						Don't have an account? 
-					</p>
+					<p>Don't have an account?</p>
 					<a href="http://localhost:3000/register">Sign Up</a>
 				</div>
 			</div>
+			<div>{message && <p>{message}</p>}</div>
 		</>
 	);
 }
