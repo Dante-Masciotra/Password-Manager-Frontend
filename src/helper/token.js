70bf426589b2f60aa130a226cef93d701277c50c
Zach <zachhutz0@gmail.com>
2023-07-18 19:18:01 -0400

    add JWT helper functions

---
+++ src/helper/token.js
@@ -0,0 +1,20 @@
+const parseJwt = (token) => {
+	const base64Url = token.split(".")[1];
+	const base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
+	const jsonPayload = decodeURIComponent(
+		window
+			.atob(base64)
+			.split("")
+			.map((c) => {
+				return "%" + ("00" + c.charCodeAt(0).toString(16)).slice(-2);
+			})
+			.join("")
+	);
+	return JSON.parse(jsonPayload);
+};
+
+export const isExpired = (token) => {
+	const decode = parseJwt(token);
+	const currentTime = Math.floor(Date.now() / 1000);
+	return decode.exp < currentTime;
+};
