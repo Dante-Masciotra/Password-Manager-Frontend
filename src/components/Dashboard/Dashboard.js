89c6989eba22e80d0e13dda8f33408d6888881b3
Zach <zachhutz0@gmail.com>
2023-07-18 22:30:15 -0400

    added user auth to dashboard

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,21 +1,58 @@
 import "./Dashboard.css";
-import React from 'react';
+import React, { useState, useEffect } from "react";
+import { authenticatePage } from "../../helper/token";
+import { useNavigate } from "react-router-dom";
 
 export default function Dashboard() {
-  return(
-    <>
-      <h2>Dashboard</h2>
-      <table>
-        <tr>
-          <th>Website</th>
-          <th>Password</th>
-        </tr>
-        <tr>
-          <td>Sample Data</td>
-          <td>Sample Data</td>
-        </tr>
-      </table>
-      <button><a href="http://localhost:3000/AddPassword"> +</a></button>
-    </>
-  );
+	const [authorized, setAuthorized] = useState(false);
+	const navigate = useNavigate();
+	useEffect(() => {
+		const checkAuthorization = async () => {
+			if (
+				await authenticatePage(
+					localStorage.getItem("token"),
+					localStorage.getItem("refresh")
+				)
+			) {
+				setAuthorized(true);
+			} else {
+				setAuthorized(false);
+				navigate("/");
+			}
+		};
+		checkAuthorization();
+		const handleStorageChange = () => {
+			checkAuthorization();
+		};
+
+		window.addEventListener("storage", handleStorageChange);
+
+		return () => {
+			window.removeEventListener("storage", handleStorageChange);
+		};
+	}, []);
+	if (!authorized)
+		return (
+			<>
+				<div>Session expired</div>
+			</>
+		);
+	return (
+		<>
+			<h2>Dashboard</h2>
+			<table>
+				<tr>
+					<th>Website</th>
+					<th>Password</th>
+				</tr>
+				<tr>
+					<td>Sample Data</td>
+					<td>Sample Data</td>
+				</tr>
+			</table>
+			<button>
+				<a href="http://localhost:3000/AddPassword"> +</a>
+			</button>
+		</>
+	);
 }
